e096fd174ef6cfffbafe994b9aead41a93b2566c
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-08-11 00:02:02 +0300

    Basic API setup

--- Back-end/api/v1/app.py
+++ Back-end/api/v1/app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""" Flask Application """
+"""Contains the https code messages handlers"""
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from api.v1.views import app_views, app_views_docs
+from api.v1.views import app_views
 
 
 app = Flask(__name__)
@@ -12,7 +12,25 @@ app.register_blueprint(app_views)
 @app.errorhandler(404)
 def not_found():
     """404 page loader"""
-    return jsonify({"Error": "Page not found! Please check the URL"}), 404
+    not_found = {"Error": "Page not found! Please check the URL"}
+    return jsonify(not_found), 404
+
+
+@app.errorhandler(500)
+def server_error():
+    """
+    Server error page loader.
+    Only for devolopers.
+    """
+    error_message = {"Error": "Server incountered an error. Pleses check logs"}
+    return jsonify(error_message), 500
+
+
+@app.route("/status", methods=["GET"], strict_slashes=False)
+def api_status():
+    """Checks if server is running"""
+    good_state = {"Status": "API running!"}
+    return jsonify(good_state), 200
 
 
 if __name__ == "__main__":
--- Back-end/api/v1/views/__init__.py
+++ Back-end/api/v1/views/__init__.py
@@ -3,6 +3,5 @@
 from flask import Blueprint, request
 
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint('app_views', __name__, url_prefix='/api')
 
-from api.v1.views.index import *
