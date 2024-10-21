ed5ba5aee0be85442d493982f98040ef4991a8e4
MegaChie <workspace.mega@gmail.com>
2024-10-21 18:14:43 +0300

    Adds 404 page

--- ency-front/src/App.js
+++ ency-front/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import NotFound from './components/NotFound'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/home" element={<Home />}></Route>
+        <Route path="/404" element={<NotFound />}></Route>
       </Routes>
     </Router>
   );
---
+++ ency-front/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import './NotFound.css';
+
+function NotFound() {
+  return (
+    <div>
+      <p>404 image goses here</p>
+    </div>
+  );
+}
+
+export default NotFound
